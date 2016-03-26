c79e54bff380a7f8fc120a30154df0e220c0e0be
Sergio Infante <raulsergio9@gmail.com>
2016-03-26 09:11:02 -0500

    Divide settings into local and production files

--- AllStars/settings/base.py
+++ AllStars/settings/base.py
@@ -22,9 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'tbu=2!7kv(ll8naf#e_e8glr!g+oz4i6lw4j0gt01qf5=67^v+'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -69,17 +66,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'AllStars.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation

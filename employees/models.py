4fb371b43f8a83fccedf26256d697006cc150111
jvaldivia <javier.valdivia@clarabridge.com>
2016-04-01 12:02:13 -0500

    added python unicode compatible decorator

--- employees/models.py
+++ employees/models.py
@@ -2,15 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
 
-
+@python_2_unicode_compatible
 class Role(models.Model):
 	name = models.CharField(max_length=100)
 
 	def __str__(self):
 		return self.name
 
+@python_2_unicode_compatible
 class Category(models.Model):
     name = models.CharField(max_length=100)
     weight = models.PositiveSmallIntegerField(default=1)
